e289533615d532bccd63de08aa82d22096f6d042
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-01-06 13:06:57 +0100

    esercizi chill

---
+++ Esercizi/src/Components/GsapTo.jsx
@@ -0,0 +1,13 @@
+const GsapTo = () => {
+  return (
+    <section>
+      <div className="mt-20">
+        <div id="blue-box" className="w-20 h-20 bg-blue-500 rounded-lg">
+          IHIHIHIH
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default GsapTo
